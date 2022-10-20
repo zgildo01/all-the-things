e90e77a2129f7dfefb2f8efe3a187e59530250d6
zgildo01 <zzaragozagildo@gmail.com>
2022-10-20 12:00:33 -0400

    gave myself a landing link pog

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,11 +5,11 @@ const Landing = () => {
     <>
       {/* All the <Link> components should live here */}
       <h1>All-The-Things</h1>
-      <Link to="/the-manliest-things">Ben's Things</Link><br/>
+      <Link to="/the-zeus-things">Zeus's Epic Things</Link><br/>
       <Link to="/the-well-styled-things">David's Things</Link><br/>
       <Link to="/the-silly-things">Hunter's Things</Link><br/>
     </>
   )
 }
- 
+
 export default Landing
