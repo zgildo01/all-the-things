8073968a3500b75f119aad381a4aad836c606287
zgildo01 <zzaragozagildo@gmail.com>
2022-10-20 11:53:07 -0400

    add Zeus things

--- src/App.js
+++ src/App.js
@@ -5,33 +5,37 @@ import SillyThings from './pages/SillyThings/SIllyThings'
 import Landing from './pages/Landing/Landing'
 
 const App = () => {
-  const bensThings = [
+  const zeusThings = [
     {
-      name: 'banana',
+      name: 'cain',
       image:
-        'https://images.unsplash.com/photo-1587132137056-bfbf0166836e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        'https://files.slack.com/files-pri/T0351JZQ0-F046ZKAENBH/20221017_130111.jpg',
       attributes: [
-        'potassium-rich',
-        'yellow when good',
-        'not a taco',
-        'ring ring ring ring ring ring ring',
+        'rotund',
+        'mad',
+        'loud',
+        'vocal'
       ],
     },
     {
-      name: 'taco',
+      name: 'Rolls Royce Phantom',
       image:
-        'https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80',
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.5bgg9ybGt7dEa1T6J2Om0AHaE7%26pid%3DApi&f=1&ipt=566c4748e713b1d8be72970255119c4b5c31540beded984a15728271e73a9181&ipo=images',
       attributes: [
-        'yummy in my tummy',
-        'salsas may vary',
-        'definitely a taco',
-        'very edible',
+        'has a neat ceiling',
+        'fast',
+        '0-60 in 5.1 secs',
+        'can zoom',
       ],
     },
     {
-      name: 'linux',
-      image: 'https://i.imgur.com/3BmfSOA.png',
-      attributes: ['not a taco', 'not Windows', 'not macOS', 'Penguins?'],
+      name: 'GTA',
+      image: 'https://download.softwsp.com/sites/13/2019/05/gta-5-12.jpg',
+      attributes: [
+        'crime', 
+        'car', 
+        'video game',
+      ],
     },
   ]
 
